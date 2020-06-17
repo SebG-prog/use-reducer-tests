0b28ac5904b27ed0215429769e41729ba4001137
SebG-prog <sebastien-gruet@orange.fr>
2020-06-17 19:27:34 +0200

    Use of a reducer function without useReducer

--- src/components/Demo.js
+++ src/components/Demo.js
@@ -1,14 +1,14 @@
 import React, { useState } from 'react'
 import Axios from 'axios'
 
-const reducer = () => {
-  switch () {
-    case '':
-      return
-    case '':
-      return
-    default
-      return
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'startGettingName':
+      return { ...state, loading: true }
+    case 'endGettingName':
+      return { ...state, name: action.name, loading: false }
+    default:
+      return state
   }
 }
 
@@ -20,19 +20,19 @@ const Demo = () => {
   const [state, setState] = useState(initialValues)
 
   const handleClick = () => {
-    // action 'startGettingName'
-    setState({...state, loading: true})
+    const action = { type: 'startGettingName' }
+    setState(reducer(state, action))
     Axios.get('name.json').then(res => {
-      // action 'endGettingName'
-      setState({...state, name: res.data.name, loading: false})
+      const action = { type: 'endGettingName', name: res.data.name }
+      setState(reducer(state, action))
     })
   }
   return (
     state.loading ? <div>Loading...</div> :
-    <main>
-      <div>{state.name}</div>
-      <button onClick={handleClick}>Click Me</button>
-    </main>
+      <main>
+        <div>{state.name}</div>
+        <button onClick={handleClick}>Click Me</button>
+      </main>
   )
 }
 
