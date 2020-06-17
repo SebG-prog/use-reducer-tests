8f466cc24e17ca046e27936d1115d490c8a5e2d6
SebG-prog <sebastien-gruet@orange.fr>
2020-06-17 18:59:36 +0200

    Version with seperate hooks

--- src/components/Demo.js
+++ src/components/Demo.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Axios from 'axios'
 
 const Demo = () => {
+  const [name, setName] = useState('Bob')
+  const [loading, setLoading] = useState(false)
+
+  const handleClick = () => {
+    setLoading(true)
+    Axios.get('name.json').then(res => {
+      setName(res.data.name)
+      setLoading(false)
+    })
+  }
   return (
-    <div>Hi</div>
+    loading ? <div>Loading...</div> :
+    <main>
+      <div>{name}</div>
+      <button onClick={handleClick}>Click Me</button>
+    </main>
   )
 }
 
